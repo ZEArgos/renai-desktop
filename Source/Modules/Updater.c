75b1e686b91eb9dfe7a0eb57aaf18f6b734e4027
ZEArgos <ais_argos@protonmail.com>
2024-06-14 18:23:52 -0400

    window close

--- Source/Modules/Updater.c
+++ Source/Modules/Updater.c
@@ -26,4 +26,21 @@ void HandleInput(KeyBuffer* buffer, Window* key_window)
                 ->value.unsigned32 -= 1;
         else RemoveMapItem(buffer->cooldown_map, GLFW_KEY_F11);
     }
+
+    void* f12_keybuffer_value =
+        GetMapItemValue(buffer->cooldown_map, GLFW_KEY_F12);
+    if (glfwGetKey(key_window->inner_window, GLFW_KEY_F12) == GLFW_PRESS &&
+        f12_keybuffer_value == NULL)
+    {
+        glfwSetWindowShouldClose(key_window->inner_window, 1);
+
+        AppendMapItem(buffer->cooldown_map, GLFW_KEY_F12, 50);
+    }
+    else if (f12_keybuffer_value != NULL)
+    {
+        if (CastVoidPointerToType(u32, f12_keybuffer_value) > 0)
+            GetMapKeyPair(buffer->cooldown_map, GLFW_KEY_F12)
+                ->value.unsigned32 -= 1;
+        else RemoveMapItem(buffer->cooldown_map, GLFW_KEY_F12);
+    }
 }
